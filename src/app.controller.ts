2b1875e5cc29c3fdd02d755961b1534d52c1b3ba
BroGames <hunempire11@gmail.com>
2024-10-02 14:30:10 +0200

    something

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ import { Response } from 'express';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  #payment: Payment[] = [];
   @Get()
   @Render('index')
   getHello() {
@@ -18,15 +19,42 @@ export class AppController {
   @Get('webshop')
   @Render('webshop')
   webshop() {
-
+    return {
+      data: {},
+      errors: []
+    }
   }
 
-  @Post('webshopFromPost')
+  @Post('webshop')
   webshopFormPost(@Body() PaymentDto: PaymentDto,@Res() response : Response) {
-    
+    console.log("coupon"+PaymentDto.coupon_code)
     let errors = []
+    if(!PaymentDto.name || !PaymentDto.bill_address || !PaymentDto.ship_address || !PaymentDto.card_number || !PaymentDto.exp_date || !PaymentDto.sec_code){
+      errors.push("A kuponon kívül minden mezőt kötelező kitölteni!")
+    }
+    if(!/^\d{4}-\d{4}-\d{4}-\d{4}$/.test(PaymentDto.card_number)){
+      errors.push("A számlaszám 0000-0000 formátumú legyen");
+    }
+    if(!/^.+, \d{4}, .+, .+, .+$/.test(PaymentDto.bill_address)){
+      errors.push("A számlázási cím (Ország, Irányítószám, Város, Utca házszám)")
+    }
+    if(!/^.+, \d{4}, .+, .+, .+$/.test(PaymentDto.ship_address)){
+      errors.push("A szállítási cím (Ország, Irányítószám, Város, Utca házszám)")
+    }
+    if(!/^[A-Z]{2}-\d{4}$/.test(PaymentDto.coupon_code) || !PaymentDto.coupon_code == null){
+      errors.push("A kuponkód helytelen")
+    }
+    if(!/^\d\d\/\d\d$/.test(PaymentDto.exp_date)){
+      errors.push("lejárati dátum hibás")
+    }
+    else if(parseInt(PaymentDto.exp_date.split('/')[1]) < new Date().getFullYear() || parseInt(PaymentDto.exp_date.split('/')[0]) < new Date().getMonth()){
+      errors.push("A kártya már lejárt")
+    }
+    if(!/^\d{3}$/.test(PaymentDto.sec_code)){
+      errors.push("Biztonsági kód hibás")
+    }
     if(errors.length > 0){
-      response.render('openAccountForm', 
+      response.render('webshop', 
         {
           data: PaymentDto,
           errors: errors
@@ -35,15 +63,16 @@ export class AppController {
       return;
     }
     const newPayment: Payment = {
-      name: 
+      name: PaymentDto.name,
+      bill_address: PaymentDto.bill_address,
+      ship_address: PaymentDto.ship_address,
+      coupon_code: PaymentDto.coupon_code,
+      card_number: PaymentDto.card_number,
+      exp_date: PaymentDto.exp_date,
+      sec_code: parseInt(PaymentDto.sec_code)
     }
-    this.#accounts.push(newAccount)
-    response.redirect('openAccountSuccess')
-
-  }
-  @Get('webshopForm')
-  @Render('payment_form')
-  webshopForm() {
+    this.#payment.push(newPayment)
+    response.redirect('paymenSuccess')
 
   }
 }
