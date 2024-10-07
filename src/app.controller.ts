0fb853912fffdd98720e2ba125e6e5af66f3005d
BroGames <hunempire11@gmail.com>
2024-10-07 12:28:05 +0200

    done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -27,27 +27,27 @@ export class AppController {
 
   @Post('webshop')
   webshopFormPost(@Body() PaymentDto: PaymentDto,@Res() response : Response) {
-    console.log("coupon"+PaymentDto.coupon_code)
     let errors = []
     if(!PaymentDto.name || !PaymentDto.bill_address || !PaymentDto.ship_address || !PaymentDto.card_number || !PaymentDto.exp_date || !PaymentDto.sec_code){
       errors.push("A kuponon kívül minden mezőt kötelező kitölteni!")
     }
     if(!/^\d{4}-\d{4}-\d{4}-\d{4}$/.test(PaymentDto.card_number)){
-      errors.push("A számlaszám 0000-0000 formátumú legyen");
+      errors.push("A számlaszám 0000-0000-0000-0000 formátumú legyen");
     }
-    if(!/^.+, \d{4}, .+, .+, .+$/.test(PaymentDto.bill_address)){
+    if(!/^.+, \d{4}, .+, .+ .+.$/.test(PaymentDto.bill_address)){
       errors.push("A számlázási cím (Ország, Irányítószám, Város, Utca házszám)")
     }
-    if(!/^.+, \d{4}, .+, .+, .+$/.test(PaymentDto.ship_address)){
+    if(!/^.+, \d{4}, .+, .+ .+.$/.test(PaymentDto.ship_address)){
       errors.push("A szállítási cím (Ország, Irányítószám, Város, Utca házszám)")
     }
-    if(!/^[A-Z]{2}-\d{4}$/.test(PaymentDto.coupon_code) || !PaymentDto.coupon_code == null){
+    if(!/^[A-Z]{2}-\d{4}$/.test(PaymentDto.coupon_code) && PaymentDto.coupon_code){
+      console.log(PaymentDto.coupon_code)
       errors.push("A kuponkód helytelen")
     }
     if(!/^\d\d\/\d\d$/.test(PaymentDto.exp_date)){
       errors.push("lejárati dátum hibás")
     }
-    else if(parseInt(PaymentDto.exp_date.split('/')[1]) < new Date().getFullYear() || parseInt(PaymentDto.exp_date.split('/')[0]) < new Date().getMonth()){
+    else if( parseInt("20"+PaymentDto.exp_date.split('/')[1]) < new Date().getFullYear() || parseInt(PaymentDto.exp_date.split('/')[0]) < new Date().getMonth()){
       errors.push("A kártya már lejárt")
     }
     if(!/^\d{3}$/.test(PaymentDto.sec_code)){
@@ -72,7 +72,13 @@ export class AppController {
       sec_code: parseInt(PaymentDto.sec_code)
     }
     this.#payment.push(newPayment)
-    response.redirect('paymenSuccess')
+    response.redirect('paymentSuccess')
+
+  }
+
+  @Get('paymentSuccess')
+  @Render('paymentSuccess')
+  paymentSuccess(){
 
   }
 }
